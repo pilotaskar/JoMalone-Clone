b7e8790d3bc565f7aa11e8e97c798373e68e52be
dootalievas <dootalievas@gmail.com>
2022-01-14 22:17:28 -0600

    added buttons, products are shown on UI, filtering function for categories

--- script.js
+++ script.js
@@ -1,21 +1,80 @@
-import {data}from './data'
-const container=document.querySelector('.container')
-const alldata=data()
-const showProducts=(array) => {
-    for(let el of array){ 
-        const div=document.createElement('div')
-        const img=documnet.createElement('img')
-        const productname=document.createElement('h3')
-        const price=document.createElement('p')
-        const size=document.createElement('p')
+import { data } from "./data.js";
+const container = document.querySelector(".container");
+const alldata = data();
+const buttons = document.querySelectorAll(".btn");
 
-       
-        price.innerHTML=el.price
-        size.innerHTML=el.size
-        productname.innerHTML=el.productName
-        img.src=el.image
+const showProducts = (array) => {
+  for (let el of array) {
+    const div = document.createElement("div");
+    div.innerHTML = `
+    <img src="${el.image}" alt="" class="productImg" />
+    <h3 class="name">${el.productName}</h3>
+    <p class= "price">${el.price}</p>
+    <p class ="size">${el.size}</p>
+  `;
+    div.className = "card";
+    container.appendChild(div);
 
-        console.log(price)
-        }
-}
+    // const img = document.createElement("img");
+    // const productName = document.createElement("h3");
+    // const price = document.createElement("p");
+    // const size = document.createElement("p");
+
+    // img.className = "productImg";
+    // productName.className = "name";
+    // price.className = "price";
+    // size.className = "size";
+
+    // price.innerHTML = el.price;
+    // size.innerHTML = el.size;
+    // productName.innerHTML = el.productName;
+    // img.src = el.image;
+
+    // div.appendChild(img);
+    // div.appendChild(productName);
+    // div.appendChild(price);
+    // div.appendChild(size);
+
+    // console.log(price, "the price");
+    // console.log(productName, "the name");
+    // console.log(img, "the images");
+  }
+};
 showProducts(alldata);
+
+const filterProducts = (arr, id) => {
+  return arr.filter((obj) => obj.category === id);
+};
+
+buttons.forEach((btn) => {
+  btn.addEventListener("click", function (e) {
+    const dataId = e.target.getAttribute("data-id");
+    if (dataId === "home essentials") {
+      clearUi();
+      const filteredProducts = filterProducts(alldata, dataId);
+      showProducts(filteredProducts);
+      console.log(filteredProducts);
+    } else if (dataId === "cologne") {
+      clearUi();
+      const filteredProducts = filterProducts(alldata, dataId);
+      showProducts(filteredProducts);
+      console.log(filteredProducts);
+    } else if (dataId === "bath and body") {
+      clearUi();
+      showProducts(filteredProducts);
+      const filteredProducts = filterProducts(alldata, dataId);
+      console.log(filteredProducts);
+    } else if (dataId === "all") {
+      clearUi();
+      showProducts(alldata);
+    }
+  });
+});
+
+///clean container function
+const clearUi = () => {
+  while (container.firstChild) {
+    container.removeChild(container.firstChild);
+  }
+};
+// clearUi();
